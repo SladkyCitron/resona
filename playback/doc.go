be287ac9cbd809d77beb7ae5198cbb19a8fb2bce
SladkyCitron <57183251+SladkyCitron@users.noreply.github.com>
2026-07-05 17:11:57 +0200

    Remove fun fact in playback package doc comment

--- playback/doc.go
+++ playback/doc.go
@@ -1,8 +1,3 @@
 // Package playback provides an interface for audio playback,
 // allowing audio to be sent to various drivers and devices.
-//
-// Fun fact: the first audio played using this package was
-// the song [Parasol Cider] by MORE MORE JUMP!.
-//
-// [Parasol Cider]: https://www.youtube.com/watch?v=KJI_3HaLtmc
 package playback
